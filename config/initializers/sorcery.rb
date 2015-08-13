0e1cdaa8cb0f85c363b50be6daa977a36bcdd30f
illsism <raul.unknown@gmail.com>
2015-08-13 11:06:03 +0300

    Remove delayed job

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -461,15 +461,5 @@ Rails.application.config.sorcery.configure do |config|
   # Define which model authenticates with sorcery.
   config.user_class = "User"
 
-  module Sorcery
-    module Model
-      module InstanceMethods
-        def generic_send_email(method, mailer)
-          config = sorcery_config
-          mail = config.send(mailer).delay.send(config.send(method), self)
-        end
-      end
-    end
-  end
 
 end
