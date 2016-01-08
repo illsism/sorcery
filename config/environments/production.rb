6d1a4d0dcd9c10b8e4317e4d4ad6b147c30b0ab5
illsism <raul.unknown@gmail.com>
2016-01-08 15:33:43 +0200

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'morning-savannah-9156.herokuapp.com'
+  host = 'sleepy-meadow-7259.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
